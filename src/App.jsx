016f3316ac2174706ef0a129d73fd284c23c9472
wissemdallege-crypto <wissemdallege@gmail.com>
2026-06-09 19:39:06 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         <button
           type="button"
           className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() => setCount((count) => count + 6415537)}
         >
           Count is {count}
         </button>
